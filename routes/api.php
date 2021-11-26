f7ed61c57d0707df09bc2929ef106a7f0f77af2d
Antony HM <antonio_hm25@hotmail.com>
2021-11-26 11:31:59 -0600

    usa cors en los endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,38 +14,41 @@ use App\Http\Controllers\CompanyCustomersController;
 use App\Http\Controllers\CompanyDebtPaymentsController;
 use App\Http\Controllers\CompanyIncomePaymentsController;
 
-Route::group(['middleware' => 'auth:api'], function () {
-    
-    Route::get('/login', [UserController::class, 'index']);
 
-    Route::post('/logout', [UserController::class, 'logout']); 
+Route::middleware(['cors'])->group(function () {
+    Route::group(['middleware' => 'auth:api'], function () {
     
-    Route::get('/branches', [BranchesController::class, 'index']);
+        Route::get('/login', [UserController::class, 'index']);
     
-    Route::get('/sales-lines', [SalesController::class, 'salesLine']);
-    Route::get('/sales-specials', [SalesController::class, 'salesSpecial']);
-
-    Route::get('/company-incomes', [CompanyIncomesController::class, 'index']);
-    Route::post('/company-incomes', [CompanyIncomesController::class, 'store']);
+        Route::post('/logout', [UserController::class, 'logout']); 
+        
+        Route::get('/branches', [BranchesController::class, 'index']);
+        
+        Route::get('/sales-lines', [SalesController::class, 'salesLine']);
+        Route::get('/sales-specials', [SalesController::class, 'salesSpecial']);
     
-    Route::get('/company-income-payments', [CompanyIncomePaymentsController::class, 'index']);
+        Route::get('/company-incomes', [CompanyIncomesController::class, 'index']);
+        Route::post('/company-incomes', [CompanyIncomesController::class, 'store']);
+        
+        Route::get('/company-income-payments', [CompanyIncomePaymentsController::class, 'index']);
+        
+        Route::get('/company-debts', [CompanyDebtsController::class, 'index']);
+        Route::post('/company-debts', [CompanyDebtsController::class, 'store']);
+        Route::put('/company-debts/{companyDebt}/approved-rejected', [CompanyDebtsController::class, 'approved']);
     
-    Route::get('/company-debts', [CompanyDebtsController::class, 'index']);
-    Route::post('/company-debts', [CompanyDebtsController::class, 'store']);
-    Route::put('/company-debts/{companyDebt}/approved-rejected', [CompanyDebtsController::class, 'approved']);
-
-    Route::get('/company-debt-payments', [CompanyDebtPaymentsController::class, 'index']);
-   
-    Route::get('/business-units', [BusinessUnitsController::class, 'index']);
+        Route::get('/company-debt-payments', [CompanyDebtPaymentsController::class, 'index']);
+       
+        Route::get('/business-units', [BusinessUnitsController::class, 'index']);
+        
+        Route::get('/company-customers', [CompanyCustomersController::class, 'index']);
+        
+        Route::get('/suppliers', [SuppliersController::class, 'index']);
     
-    Route::get('/company-customers', [CompanyCustomersController::class, 'index']);
+        Route::get('/inventories', [InventoryController::class, 'index']);
     
-    Route::get('/suppliers', [SuppliersController::class, 'index']);
-
-    Route::get('/inventories', [InventoryController::class, 'index']);
-
-    Route::get('branch-safe-boxes', [BranchSafeBoxesController::class, 'index']);
-    Route::post('branch-safe-boxes/{branchSafeBox}/cash-withdrawal', [BranchSafeBoxesController::class, 'cashWithdrawal']);
+        Route::get('branch-safe-boxes', [BranchSafeBoxesController::class, 'index']);
+        Route::post('branch-safe-boxes/{branchSafeBox}/cash-withdrawal', [BranchSafeBoxesController::class, 'cashWithdrawal']);
+    });
+    
+    Route::post('/login', [UserController::class, 'login']);
 });
-
-Route::post('/login', [UserController::class, 'login']);
