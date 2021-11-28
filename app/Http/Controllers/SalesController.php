b0fa9f5b3127df3d13255a5a16c5e62de14d5d54
Antony HM <antonio_hm25@hotmail.com>
2021-11-27 22:44:15 -0600

    correción en consulta de ventas especiales

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -16,11 +16,11 @@ class SalesController extends Controller
             'startDate' => 'required|date',
             'endDate' => 'required|date'
         ]);
-        
+
         $amount = $this->salesLineQuery($request)
             ->select('total')
             ->sum('total');
-        
+
         $sales = $this->salesLineQuery($request)
             ->select('sale_date','payment_method','branch_id','total')
             ->get();
@@ -31,20 +31,15 @@ class SalesController extends Controller
         ], 200);
     }
 
-    public function salesSpecial(Request $request) 
+    public function salesSpecial(Request $request)
     {
         $request->validate([
             'branchId' => 'required|integer|exists:branches,id',
+            'skip' => 'required|integer',
             'startDate' => 'required|date',
             'endDate' => 'required|date'
         ]);
 
-        $amount = $this->salesSpecialQuery($request)
-            ->select(
-                'sales.total' 
-            )
-            ->sum('sales.total');
-        
         $sales = $this->salesSpecialQuery($request)
             ->select(
                 'sales.sale_date',
@@ -52,13 +47,15 @@ class SalesController extends Controller
                 'sales.branch_id',
                 'special_product_order_payments.amount',
                 'special_product_orders.cake_type',
-                'sales.total' 
+                'sales.total'
             )
+            ->skip($request->skip)
+            ->take(15)
             ->get();
 
 
         return response()->json([
-            'amount'=> $amount,
+            'amount'=>  $sales->sum('total'),
             'sales' => $sales
         ], 200);
     }
@@ -69,7 +66,7 @@ class SalesController extends Controller
         ->where('invisible', 0)
         ->WhereBetween('sale_date',[$request->startDate, $request->endDate]);
     }
-    
+
     private function salesSpecialQuery(Request $request) {
         return Sale::join('special_product_orders', 'sales.branch_id', '=', 'special_product_orders.branch_id')
         ->join('special_product_order_payments', 'special_product_orders.id', '=', 'special_product_order_payments.special_product_order_id')
