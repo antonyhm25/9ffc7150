bf44d06ae7a5d533abdf1992424a3761390718fd
Antony HM <antonio_hm25@hotmail.com>
2021-11-27 19:32:45 -0600

    agrega soporte para consultar regalias mensuales y anuales

--- app/Http/Controllers/BranchRoyaltiesController.php
+++ app/Http/Controllers/BranchRoyaltiesController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\BranchRoyalties\BranchRoyaltyResult;
+use App\Models\BranchAnnualRoyalty;
 use App\Models\BranchRoyalty;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class BranchRoyaltiesController extends Controller
 {
     public function index(Request $request)
     {
+        $response = null;
+
         $request->validate([
-            'branchId' => 'required|exists:branches,id'
+            'branchId' => 'required|exists:branches,id',
+            'isAnual' => [
+                'required',
+                Rule::in([0, 1])
+            ]
         ]);
 
-        $response = BranchRoyalty::where('branch_id', $request->branchId)->get();
+        if ($request->boolean('isAnual')) {
+            $response = BranchAnnualRoyalty::where('branch_id', $request->branchId)->get();
+        } else {
+            $response = BranchRoyalty::where('branch_id', $request->branchId)->get();
+        }
 
         return response()->json(BranchRoyaltyResult::collection($response));
     }
